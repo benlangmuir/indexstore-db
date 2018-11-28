7c494f84cc9c300ac3e834a9d463ca7b2eb107da
Ben Langmuir <blangmuir@apple.com>
2018-11-28 14:05:49 -0800

    WIP: builds\!

--- Utilities/build-toolchain-swift-package.py
+++ Utilities/build-toolchain-swift-package.py
@@ -15,6 +15,7 @@ import errno
 import os
 import platform
 import shutil
+import subprocess
 import sys
 
 LIB_SUFFIX = 'dylib' if platform.system() == 'Darwin' else 'so'
@@ -24,6 +25,7 @@ verbose = False
 def main():
   parser = argparse.ArgumentParser(description='Build a SwiftPM package as part of the Swift toolchain build.')
   parser.add_argument('-v', '--verbose', action='store_true', help='use verbose output')
+  parser.add_argument('build_actions', help="extra actions to perform (test)", nargs="*", default=[])
 
   swiftpm_group = parser.add_argument_group('swift package options')
   swiftpm_group.add_argument('--build-path',
@@ -37,20 +39,22 @@ def main():
       default='debug', metavar='CONFIG')
 
   toolchain_group = parser.add_argument_group('toolchain options')
-  toolchain_group.add_argument('--swiftc', dest='swiftc_path',
-      help='path to the swift compiler',
-      metavar='PATH')
+  toolchain_group.add_argument('--swift', dest='swift_path',
+      help='path to the swift executable',
+      metavar='PATH', required=True)
   toolchain_group.add_argument('--swift-build', dest='swift_build_path',
       help='path to the swift-build exectuable',
+      metavar='PATH')
+  toolchain_group.add_argument('--swiftpm-bootstrap', dest='swiftpm_bootstrap_path',
+      help='path to the swiftpm .bootstrap directory',
       metavar='PATH')
   toolchain_group.add_argument('--swift-build-tool', dest='swift_build_tool_path',
       help='path to the swift-build-tool exectuable',
       metavar='PATH')
-  toolchain_group.add_argument('--swift-test', dest='swift_test_path',
-      help='path to the swift-test exectuable',
-      metavar='PATH')
-  toolchain_group.add_argument('--foundation', dest='foundation_path',
+  toolchain_group.add_argument('--foundation-build-dir',
       help='path to Foundation build directory')
+  toolchain_group.add_argument('--foundation-source-dir',
+      help='path to Foundation source directory')
   toolchain_group.add_argument('--xctest', dest='xctest_path',
       help='path to XCTest build directory')
   toolchain_group.add_argument('--libdispatch-build-dir',
@@ -60,6 +64,11 @@ def main():
 
   args = parser.parse_args()
 
+  # Validate the build actions.
+  for action in args.build_actions:
+      if action not in ('test'):
+          raise SystemExit("unknown build action: {}".format(action))
+
   global verbose
   verbose = args.verbose
 
@@ -68,7 +77,21 @@ def main():
 
   make_fake_toolchain(toolchain_root, args)
 
-  # build, test
+  swift = os.path.join(toolchain_root, 'usr', 'bin', 'swift')
+  if 'test' in args.build_actions:
+    swiftpm_cmd(swift, 'test', args, toolchain_root)
+  else:
+    swiftpm_cmd(swift, 'build', args, toolchain_root)
+
+def swiftpm_cmd(swift, verb, args, toolchain_root):
+  cmd = [swift, verb]
+
+  if platform.system() != 'Darwin':
+    cmd += ['-Xcxx', '-I', '-Xcxx', os.path.join(toolchain_root, 'usr', 'lib', 'swift')]
+
+  if verbose:
+    print(' '.join(cmd))
+  subprocess.check_call(cmd, stderr=subprocess.STDOUT)
 
 def make_fake_toolchain(root, args):
   if os.path.exists(root):
@@ -85,7 +108,7 @@ def make_fake_toolchain(root, args):
     print('Unknown host platform')
     sys.exit(1)
   # FIXME: hardcoded arch
-  module_path = os.path.join(lib_swift_path, 'x86_64')
+  module_path = os.path.join(lib_swift_host_path, 'x86_64')
 
   makedirs_force(bin_path)
   makedirs_force(module_path)
@@ -96,21 +119,26 @@ def make_fake_toolchain(root, args):
     elif target:
       print('Skip {}'.format(target))
 
-  if args.swiftc_path:
-    # swift cannot be a symlink, because the resource directory is resolved
-    # relative to the binary's real path.
-    clonefile(args.swiftc_path, os.path.join(bin_path, 'swift'))
-    symlink_force('swift', os.path.join(bin_path, 'swiftc'))
-    target_bin = os.path.dirname(args.swiftc_path)
-    target_lib = os.path.normpath(os.path.join(target_bin, '..',  'lib'))
+  # swift cannot be a symlink, because the resource directory is resolved
+  # relative to the binary's real path.
+  clonefile(args.swift_path, os.path.join(bin_path, 'swift'))
+  symlink_force('swift', os.path.join(bin_path, 'swiftc'))
+  target_bin = os.path.dirname(args.swift_path)
+  target_lib = os.path.normpath(os.path.join(target_bin, '..',  'lib'))
 
-    copy_or_symlink_recursive(os.path.join(target_lib, 'swift'), lib_swift_path)
-    maybe_copy_or_symlink(os.path.join(target_lib, 'sourcekitd.framework'), lib_path)
-    maybe_copy_or_symlink(os.path.join(target_lib, 'libsourcekitdInProc.{}'.format(LIB_SUFFIX)), lib_path)
+  copy_or_symlink_recursive(os.path.join(target_lib, 'swift'), lib_swift_path)
+  maybe_copy_or_symlink(os.path.join(target_lib, 'sourcekitd.framework'), lib_path)
+  maybe_copy_or_symlink(os.path.join(target_lib, 'libsourcekitdInProc.{}'.format(LIB_SUFFIX)), lib_path)
 
-  maybe_copy_or_symlink(args.swift_build_path, os.path.join(bin_path, 'swift-build'))
-  maybe_copy_or_symlink(args.swift_test_path, os.path.join(bin_path, 'swift-test'))
+  maybe_copy_or_symlink(args.swift_build_tool_path, os.path.join(bin_path, 'swift-build-tool'))
 
+  if args.swift_build_path:
+    maybe_copy_or_symlink(args.swift_build_path, os.path.join(bin_path, 'swift-build'))
+    dir_path = os.path.join(os.path.dirname(args.swift_build_path))
+    maybe_copy_or_symlink(os.path.join(dir_path, 'swift-test'), os.path.join(bin_path, 'swift-test'))
+
+  if args.swiftpm_bootstrap_path:
+    maybe_copy_or_symlink(os.path.join(args.swiftpm_bootstrap_path, 'lib', 'swift', 'pm'), lib_swift_path)
 
   if args.libdispatch_source_dir:
     maybe_copy_or_symlink(os.path.join(args.libdispatch_source_dir, 'dispatch'), lib_swift_path)
@@ -123,17 +151,21 @@ def make_fake_toolchain(root, args):
     maybe_copy_or_symlink(os.path.join(args.libdispatch_build_dir, 'src', 'swift', 'Dispatch.swiftmodule'), module_path)
     maybe_copy_or_symlink(os.path.join(args.libdispatch_build_dir, 'src', 'swift', 'Dispatch.swiftdoc'), module_path)
 
-  if args.foundation_path:
-    maybe_copy_or_symlink(os.path.join(args.foundation_path, 'libFoundation.{}'.format(LIB_SUFFIX)), lib_swift_host_path)
-    maybe_copy_or_symlink(os.path.join(args.foundation_path, 'swift', 'Foundation.swiftmodule'), module_path)
-    maybe_copy_or_symlink(os.path.join(args.foundation_path, 'swift', 'Foundation.swiftdoc'), module_path)
-    maybe_copy_or_symlink(os.path.join(args.foundation_path, 'CoreFoundation-prefix', 'System', 'Library', 'Frameworks', 'CoreFoundation.framework'), module_path)
+  if args.foundation_build_dir:
+    maybe_copy_or_symlink(os.path.join(args.foundation_build_dir, 'libFoundation.{}'.format(LIB_SUFFIX)), lib_swift_host_path)
+    maybe_copy_or_symlink(os.path.join(args.foundation_build_dir, 'swift', 'Foundation.swiftmodule'), module_path)
+    maybe_copy_or_symlink(os.path.join(args.foundation_build_dir, 'swift', 'Foundation.swiftdoc'), module_path)
+
+    cf_in_path = os.path.join(args.foundation_build_dir, 'CoreFoundation-prefix', 'System', 'Library', 'Frameworks', 'CoreFoundation.framework')
+    cf_out_path = os.path.join(lib_swift_path, 'CoreFoundation')
+    makedirs_force(cf_out_path)
+    copy_or_symlink_recursive(os.path.join(cf_in_path, 'Headers'), cf_out_path)
+    maybe_copy_or_symlink(os.path.join(args.foundation_source_dir, 'CoreFoundation', 'Base.subproj', 'module.map'), os.path.join(lib_swift_path, 'CoreFoundation'))
 
   if args.xctest_path:
     maybe_copy_or_symlink(os.path.join(args.xctest_path, 'libXCTest.{}'.format(LIB_SUFFIX)), lib_swift_host_path)
     maybe_copy_or_symlink(os.path.join(args.xctest_path, 'swift', 'XCTest.swiftmodule'), module_path)
     maybe_copy_or_symlink(os.path.join(args.xctest_path, 'swift', 'XCTest.swiftdoc'), module_path)
-
 
 def copy_or_symlink_recursive(target_dir, link_dir):
   for root, dirs, files in os.walk(target_dir):
