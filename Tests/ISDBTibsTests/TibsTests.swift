e480a3772b8a0fe2ae539fcce879da22ee81f127
Ben Langmuir <blangmuir@apple.com>
2019-07-12 14:23:46 -0700

    [test] Test SwiftModule and ClangTU resolution

--- Tests/ISDBTibsTests/TibsTests.swift
+++ Tests/ISDBTibsTests/TibsTests.swift
@@ -86,7 +86,111 @@ final class TibsTests: XCTestCase {
       src.appendingPathComponent("b.swift", isDirectory: false),
     ])
 
-    // TODO: clangTUs
+    let clangTUs = target.clangTUs.sorted(by: { $0.outputPath < $1.outputPath })
+    XCTAssertEqual(clangTUs.count, 4)
+    if let tu = clangTUs.count >= 2 ? clangTUs[0] : nil{
+      XCTAssertEqual(tu.source, src.appendingPathComponent("b.c", isDirectory: false))
+      XCTAssertEqual(tu.outputPath, "main-b.c.o")
+      XCTAssertEqual(tu.generatedHeaderDep, "main.swiftmodule")
+      XCTAssertEqual(tu.importPaths, [".", "/src"])
+      XCTAssertEqual(tu.extraArgs, ["-Wno-objc-root-class"])
+    } else { XCTFail() }
+    if let tu = clangTUs.count >= 2 ? clangTUs[1] : nil {
+      XCTAssertEqual(tu.source, src.appendingPathComponent("c.m", isDirectory: false))
+      XCTAssertEqual(tu.outputPath, "main-c.m.o")
+      XCTAssertEqual(tu.generatedHeaderDep, "main.swiftmodule")
+      XCTAssertEqual(tu.importPaths, [".", "/src"])
+      XCTAssertEqual(tu.extraArgs, ["-Wno-objc-root-class"])
+    } else { XCTFail() }
+    if let tu = clangTUs.count >= 3 ? clangTUs[2] : nil {
+      XCTAssertEqual(tu.source, src.appendingPathComponent("d.cpp", isDirectory: false))
+      XCTAssertEqual(tu.outputPath, "main-d.cpp.o")
+      XCTAssertEqual(tu.generatedHeaderDep, "main.swiftmodule")
+      XCTAssertEqual(tu.importPaths, [".", "/src"])
+      XCTAssertEqual(tu.extraArgs, ["-Wno-objc-root-class"])
+    } else { XCTFail() }
+    if let tu = clangTUs.count >= 4 ? clangTUs[3] : nil {
+      XCTAssertEqual(tu.source, src.appendingPathComponent("e.mm", isDirectory: false))
+      XCTAssertEqual(tu.outputPath, "main-e.mm.o")
+      XCTAssertEqual(tu.generatedHeaderDep, "main.swiftmodule")
+      XCTAssertEqual(tu.importPaths, [".", "/src"])
+      XCTAssertEqual(tu.extraArgs, ["-Wno-objc-root-class"])
+    } else { XCTFail() }
+  }
+
+  func testResolutionSwiftModules() throws {
+    let dir = projectDir("SwiftModules")
+    let m = try manifest(projectDir: dir)
+    let tc = TibsTests.fakeToolchain
+    let src = URL(fileURLWithPath: "/src", isDirectory: true)
+    let build = URL(fileURLWithPath: "/build", isDirectory: true)
+    let builder = try TibsBuilder(manifest: m, sourceRoot: src, buildRoot: build, toolchain: tc)
+
+    XCTAssertEqual(3, builder.targets.count)
+    if builder.targets.count != 3 { return }
+
+    do {
+      let target = builder.targets[0]
+      XCTAssertEqual(target.name, "A")
+      XCTAssertEqual(target.dependencies, [])
+      XCTAssertNotNil(target.swiftModule)
+      guard let module = target.swiftModule else {
+        return
+      }
+      XCTAssertEqual(module.name, "A")
+      XCTAssertEqual(module.emitModulePath, "A.swiftmodule")
+      XCTAssertNil(module.emitHeaderPath)
+      XCTAssertNil(module.bridgingHeader)
+      XCTAssertEqual(module.moduleDeps, [])
+      XCTAssertEqual(module.importPaths, [])
+      XCTAssertEqual(module.extraArgs, [])
+      XCTAssertEqual(module.sources, [
+        src.appendingPathComponent("a.swift", isDirectory: false),
+      ])
+      XCTAssertEqual(target.clangTUs, [])
+    }
+
+    do {
+      let target = builder.targets[1]
+      XCTAssertEqual(target.name, "B")
+      XCTAssertEqual(target.dependencies, ["A"])
+      XCTAssertNotNil(target.swiftModule)
+      guard let module = target.swiftModule else {
+        return
+      }
+      XCTAssertEqual(module.name, "B")
+      XCTAssertEqual(module.emitModulePath, "B.swiftmodule")
+      XCTAssertNil(module.emitHeaderPath)
+      XCTAssertNil(module.bridgingHeader)
+      XCTAssertEqual(module.moduleDeps, ["A.swiftmodule"])
+      XCTAssertEqual(module.importPaths, ["."])
+      XCTAssertEqual(module.extraArgs, [])
+      XCTAssertEqual(module.sources, [
+        src.appendingPathComponent("b.swift", isDirectory: false),
+      ])
+      XCTAssertEqual(target.clangTUs, [])
+    }
+
+    do {
+      let target = builder.targets[2]
+      XCTAssertEqual(target.name, "C")
+      XCTAssertEqual(target.dependencies, ["B"])
+      XCTAssertNotNil(target.swiftModule)
+      guard let module = target.swiftModule else {
+        return
+      }
+      XCTAssertEqual(module.name, "C")
+      XCTAssertEqual(module.emitModulePath, "C.swiftmodule")
+      XCTAssertNil(module.emitHeaderPath)
+      XCTAssertNil(module.bridgingHeader)
+      XCTAssertEqual(module.moduleDeps, ["B.swiftmodule"])
+      XCTAssertEqual(module.importPaths, ["."])
+      XCTAssertEqual(module.extraArgs, [])
+      XCTAssertEqual(module.sources, [
+        src.appendingPathComponent("c.swift", isDirectory: false),
+      ])
+      XCTAssertEqual(target.clangTUs, [])
+    }
   }
 
   // Compilation Database tests
